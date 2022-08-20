82482f84663cbbee10014dc052de5758ad740d56
mafking <mafsteve@gmail.com>
2022-08-20 14:21:59 +0100

    edited requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o*mb^jfwfw10e9qmwlmhh0(ydy!jg_p*a@v3+r!7@^3%#!31t1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','localhost']
 
 
 # Application definition
